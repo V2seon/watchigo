9fbf81b78f5bc6b7ffdb77397e63fc144f3df3a4
GyeongSeok-Oh <dhrudtjr945@gmail.com>
2022-12-22 16:43:56 +0900

    Oh gyeong seok

--- src/main/java/com/example/watchigo/controller/AIvideoController.java
+++ src/main/java/com/example/watchigo/controller/AIvideoController.java
@@ -2,8 +2,6 @@ package com.example.watchigo.controller;
 
 import com.example.watchigo.common.Pagination;
 import com.example.watchigo.common.SessionCheck;
-import com.example.watchigo.entity.ServiceZoneEntity;
-import com.example.watchigo.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,15 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping
+@RequestMapping("/aivideo")
 public class AIvideoController {
 
-    @GetMapping("/aivideo")
-    public String main(Model m, HttpServletRequest request, Model model){
+    @GetMapping("/list")
+    public String main(HttpServletRequest request, Model model){
 //        String returnValue = "";
 //        if(new SessionCheck().loginSessionCheck(request)){
             model.addAttribute("nowurl0", "/aivideo");
@@ -34,6 +33,14 @@ public class AIvideoController {
 //        return returnValue;
 
         return  "AIvideo.html";
+
+    }
+
+    @GetMapping("/write")
+    public String ailistenter(Model model, HttpServletRequest request) {
+        SessionCheck sessionCheck = new SessionCheck();
+        model.addAttribute("nowurl0", "/aivideo");
+        return "AIvideoInsert.html";
 
     }
 }
